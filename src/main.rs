979a029f9848bdb87bec284713118151b0d25067
Guy Ellis <guy.ellis@walmartlabs.com>
2021-07-30 14:34:36 -0700

    Create factories

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,28 @@ fn print_duration(timer: Box<dyn Timer>) {
     println!("{}", timer.duration());
 }
 
+fn real_factory() -> Box<dyn Timer> {
+    Box::new(RealTimer {
+        start_time: Instant::now(),
+    })
+}
+
+fn fake_factory() -> Box<dyn Timer> {
+    Box::new(FakeTimer {
+        time_taken: 0.444,
+    })
+}
+
+struct Container {
+    timer_factory: Box<dyn Fn() -> Box<dyn Timer>>,
+}
+
+fn print_container(container: &Container) {
+    let f = &container.timer_factory;
+    let timer = f();
+    print_duration(timer);
+}
+
 fn main() {
     let real_timer = Box::new(RealTimer {
         start_time: Instant::now(),
@@ -35,7 +57,17 @@ fn main() {
     print_duration(real_timer);
 
     let fake_timer = Box::new(FakeTimer {
-        time_taken: 0.5,
+        time_taken: 0.555,
     });
     print_duration(fake_timer);
+
+    let real_container = Container {
+        timer_factory: Box::new(real_factory),
+    };
+    print_container(&real_container);
+
+    let fake_container = Container {
+        timer_factory: Box::new(fake_factory),
+    };
+    print_container(&fake_container);
 }
